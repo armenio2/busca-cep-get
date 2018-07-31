f3e66f30dfe31bf5f55e488360a7f1e334d27732
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-07-31 20:12:41 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,38 @@
+function procuracep(){
+    console.log("procurando cep");
+    var cep = document.getElementById("cepDigitado").value;
+    cep = cep.replace("-","");   
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://viacep.com.br/ws/"+ cep +"/json/";
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var texte = JSON.parse(this.responseText);
+            console.log(texte);
+            
+        }else{
+            document.getElementById("cidade").innerText = "Cep Invalido";
+            document.getElementById("bairro").innerText = "";
+            document.getElementById("rua").innerText = "";
+        }
+        var cep2 = texte.cep;
+        cep2 = cep2.replace("-","");
+        if (cep == cep2){
+            document.getElementById("cidade").innerText = texte.localidade;
+            document.getElementById("bairro").innerText = texte.bairro;
+            document.getElementById("rua").innerText = texte.logradouro;
+        }else {
+            console.log("fudeu");
+            
+        }
+
+
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    
+
+    
+    console.log(cep);
+    
+}
